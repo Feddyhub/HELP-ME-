024c3db9492f6946b8745f9f131a5aad14e6cccf
Murat Ferhat Derya <91869273+Feddyhub@users.noreply.github.com>
2024-05-07 18:28:59 +0300

    Add files via upload
    entire entity

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Carkifelek
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox2 = new TextBox();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(665, 217);
+            button1.Name = "button1";
+            button1.Size = new Size(123, 33);
+            button1.TabIndex = 1;
+            button1.Text = "Calistir";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(665, 327);
+            button2.Name = "button2";
+            button2.Size = new Size(123, 34);
+            button2.TabIndex = 2;
+            button2.Text = "Kelime Degis";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Cooper Black", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(111, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(617, 32);
+            label1.TabIndex = 3;
+            label1.Text = "SANA VERDIGIMIZ HARFLER: B,C,D,Y,Z,I";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Cooper Black", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.WindowText;
+            label2.Location = new Point(209, 71);
+            label2.Name = "label2";
+            label2.Size = new Size(346, 26);
+            label2.TabIndex = 4;
+            label2.Text = "3 SESSIZ 1 SESLI HARF SEC";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(209, 112);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(75, 82);
+            textBox1.TabIndex = 5;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(290, 112);
+            textBox5.Multiline = true;
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(75, 82);
+            textBox5.TabIndex = 11;
+            textBox5.TextChanged += textBox5_TextChanged;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(371, 112);
+            textBox6.Multiline = true;
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(75, 82);
+            textBox6.TabIndex = 12;
+            textBox6.TextChanged += textBox6_TextChanged;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(468, 112);
+            textBox7.Multiline = true;
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(75, 82);
+            textBox7.TabIndex = 13;
+            textBox7.TextChanged += textBox7_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Verdana", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(157, 237);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(481, 176);
+            textBox2.TabIndex = 14;
+            textBox2.Text = "------------------------------";
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(12, 237);
+            button3.Name = "button3";
+            button3.Size = new Size(123, 34);
+            button3.TabIndex = 15;
+            button3.Text = "Harf Kontrol";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox2;
+        private Button button3;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Carkifelek
+{
+    public partial class Form1 : Form
+    {
+        string[] autoinput = { "b", "c", "d", "y", "z", "i" };
+        string[] input = new string[4];
+        string[] data = { "NAMAGLUPLUK", "klavye", "icardi", "holokost", "hayaletsevgilim", "Tutusturtusumuzunmus" };
+        string[] vowels = { "a", "e", "o", "u" };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            // Temizleme
+            Array.Clear(input, 0, input.Length);
+
+            Random random = new Random();
+            int a = 0;
+            bool[] flags = new bool[4];
+
+            // TextBox'lardaki harfleri kontrol etme
+            for (int i = 0; i < 4; i++)
+            {
+                TextBox textBox = Controls.Find("textBox" + (i + 1), true).FirstOrDefault() as TextBox;
+                if (textBox == null)
+                    continue;
+
+                if (textBox.Text.Length != 1)
+                {
+                    MessageBox.Show($"Lütfen 1 harf giriniz. HATA ({i + 1}): {textBox.Text}");
+                }
+                else
+                {
+                    while (!flags[i] && a < vowels.Length)
+                    {
+                        if (textBox.Text == vowels[a])
+                        {
+                            flags[i] = true;
+                        }
+                        else
+                        {
+                            a++;
+                            input[i] = textBox.Text;
+                        }
+                    }
+
+                    if (flags[i])
+                    {
+                        MessageBox.Show($"Lütfen geçerli sessiz harf giriniz {i + 1}: ");
+                    }
+                    else
+                    {
+                        MessageBox.Show($"Başarılı ({i + 1}) : ");
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            string randomData = data[random.Next(data.Length)];
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string message = string.Join("", input);
+            MessageBox.Show(message);
+        }
+    }
+}
